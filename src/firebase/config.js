55c929f8e93bb1e7ccdcd917f31e1e70bc65b33e
Kaz1787 <jiaxing.yan.2020@sis.smu.edu.sg>
2022-08-07 20:24:21 +0800

    config edit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBICg3ePdgPlxh-tT-d8FuyR1n0kVUVpBY",
-    authDomain: "esd-telehealth-patient.firebaseapp.com",
-    databaseURL: "https://esd-telehealth-patient-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "esd-telehealth-patient",
-    storageBucket: "esd-telehealth-patient.appspot.com",
-    messagingSenderId: "281674434514",
-    appId: "1:281674434514:web:5afd08d1795afbcc486a0b",
-    measurementId: "G-5K8RTWTJ77"
+	// FIREBASE CONFIG HERE
 };
 
 initializeApp(firebaseConfig);
